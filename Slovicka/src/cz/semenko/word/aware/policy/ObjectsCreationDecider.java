76a4b254b520b31930930322bf029a79a6cd596d
k <semenko.k@seznam.cz>
2013-07-05 21:04:23 +0200

    Refactoring

--- Slovicka/src/cz/semenko/word/aware/policy/ObjectsCreationDecider.java
+++ Slovicka/src/cz/semenko/word/aware/policy/ObjectsCreationDecider.java
@@ -7,63 +7,71 @@ import cz.semenko.word.aware.Thought;
 import cz.semenko.word.persistent.Associations;
 import cz.semenko.word.technology.memory.fast.FastMemory;
 
+/**
+ * Byznys process, který rozhoduje, zda vytvářet objekty nebo nikoliv.
+ * Business process to decide for object creation.
+ * 
+ * @author Kyrylo Semenko
+ *
+ */
 public class ObjectsCreationDecider {
 // Komponenty doda Spring FW
 private FastMemory fastMemory;
 private Config config;
 	
-	public ObjectsCreationDecider() {
-		;
-	}
-
 	/**
-	 * @param config the config to set
+	 * Empty constructor
 	 */
+	public ObjectsCreationDecider() {}
+
 	public void setConfig(Config config) {
 		this.config = config;
 	}
 
-	/**
-	 * @param fastMemory the fastMemory to set
-	 */
 	public void setFastMemory(FastMemory fastMemory) {
 		this.fastMemory = fastMemory;
 	}
 	/**
 	 * Rozhodnout ktere Thoughts sloucit na zaklade konfiguracnich parametru.
-	 * @param thoughts2 - Jiz spojene a nove Thoughts.
-	 * @return Vector pozici prvnich polozek thoughts2, ktere povazuje za vhodne spojit s nasledujicimy.
+	 * Decide which Thoughts to be merge. Decision is based on the configuration parameters.
+	 * 
+	 * @param thoughts - Jiz spojene a/nebo nove Thoughts.
+	 * Already merged and/or new Thoughts.
+	 * 
+	 * @return kolekce pozici prvnich polozek thoughts, ktere povazuje za vhodne spojit se sousednimy vpravo.
+	 * Collection of positions of first items, that will be merged to their right neighbours.
+	 * 
 	 * @throws Exception 
 	 */
 	public Vector<Integer> getPositionsToCreateNewObjects(
-			Vector<Thought> thoughts2) throws Exception {
+			Vector<Thought> thoughts) throws Exception {
 		Vector<Integer> result = new Vector<Integer>();
-		boolean isCreateNewObjectsToAllPairs = 
-			config.isObjectsCreationDecider_createNewObjectsToAllPairs();
-		int createNewObjectsToAllPairsDepth =
-			config.getObjectsCreationDecider_createNewObjectsToAllPairsDepth();
+		boolean isCreateNewObjectsToAllPairs = config.isObjectsCreationDecider_createNewObjectsToAllPairs();
 		if (isCreateNewObjectsToAllPairs == false) {
 			return result;
 		}
-		// Else
-		/** Dohledat vsechny mozne kombinace pro spojeni */
+
+		int createNewObjectsToAllPairsDepth = config.getObjectsCreationDecider_createNewObjectsToAllPairsDepth();
+		// Dohledat vsechny mozne kombinace pro spojeni
+		// Trace all possible combinations to merge
 		Vector<Thought> thoughtsPairToUnion = new Vector<Thought>();
-		for (int i = 0; i < thoughts2.size()-1; i++) {
-			Thought th1 = thoughts2.get(i);
-			Thought th2 = thoughts2.get(i+1);
-			if (th1.getActiveObject().getType() <= createNewObjectsToAllPairsDepth 
-					&& th2.getActiveObject().getType() <= createNewObjectsToAllPairsDepth) {
-				thoughtsPairToUnion.add(th1);
-				thoughtsPairToUnion.add(th2);
+		for (int i = 0; i < thoughts.size()-1; i++) {
+			Thought th = thoughts.get(i);
+			Thought nextTh = thoughts.get(i+1);
+			if (th.getActiveObject().getType() <= createNewObjectsToAllPairsDepth 
+					&& nextTh.getActiveObject().getType() <= createNewObjectsToAllPairsDepth) {
+				thoughtsPairToUnion.add(th);
+				thoughtsPairToUnion.add(nextTh);
 			}			
 		}
 		if (thoughtsPairToUnion.size() > 0) {
-			/** Dohledat existujici asociace */
+			// Dohledat existujici asociace
+			// Trace an existing associations
 			Vector<Associations> associations = fastMemory.getAssociations(thoughtsPairToUnion);
 			if (associations.contains(null)) {
-				for (int i = 0; i < associations.size(); i++) { // najde aspon jednou null? TODO zdokumentovat
+				for (int i = 0; i < associations.size(); i++) {
 					if (associations.get(i) == null) {
-						result.add(i * 2); // vraci pozice v thoughts2
+						result.add(i * 2); // Pozice v thoughts. Position in thoughts
 					}
 				}
 			}
