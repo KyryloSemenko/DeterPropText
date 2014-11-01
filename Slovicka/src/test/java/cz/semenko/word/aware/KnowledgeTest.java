161e64f6df970c867a8fd7595a9543e0a4bc8948
Kyrylo Semenko <semenko.k@seznam.cz>
2014-11-01 09:21:08 +0100

    Request for new test implementation

--- Slovicka/src/test/java/cz/semenko/word/aware/KnowledgeTest.java
+++ Slovicka/src/test/java/cz/semenko/word/aware/KnowledgeTest.java
@@ -24,30 +24,37 @@ public class KnowledgeTest {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		ctx = ApplicationContextProvider.getUnitTestApplicationContext();
-		setUpDataBase();
 	}
 
 	/**
-	 * Set up connection to DB server, create tables and constraints
-	 */
-	private static void setUpDataBase() throws Exception {
-//		Resource resource = ctx.getResource("classpath:cz/semenko/word/sql/createTables.sql");
-//		JdbcTestUtils.executeSqlScript(new JdbcTemplate((DataSource)ctx.getBean("dataSource")), resource, false);
-		
-		//LazyConnectionDataSourceProxy dbProxy = (LazyConnectionDataSourceProxy)ctx.getBean("database.dataSource");
-	}
-
-	/**
-	 * Test method for {@link cz.semenko.word.aware.Knowledge#remember(java.lang.Long[])}.
+	 * Při čtení aplikace předpovídá budoucí text. <br>
+	 * During reading application predicts future text. <br>
 	 */
 	@Test
 	public final void testRemember() {
 		Knowledge knowledge = ctx.getBean(Knowledge.class);
 		FastMemory memory = ctx.getBean(FastMemory.class);
 		try {
-			Long[] testData = memory.getObjects(new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g'});
+			Long[] testData = memory.getObjects("abcdefg".toCharArray());
 			knowledge.remember(testData);
-			// TODO dopsat tento test
+			testData = memory.getObjects("abcdefg".toCharArray());
+			// Jakmile Knowledge precte pismeno a, ihned nabidne odhadovane pokracovani.
+			// ab, ab-cd, abcd-ef, razene dle COST
+			knowledge.remember(testData);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+	
+	/**
+	 * Při čtení textu, COST associations roste jen do výše definované v konfiguraci.<br>
+	 * During reading of the text, association's COST increase up to configuration parameter value only.<br>
+	 */
+	@Test
+	public final void testCostIncrease() {
+		Knowledge knowledge = ctx.getBean(Knowledge.class);
+		FastMemory memory = ctx.getBean(FastMemory.class);
+		try {
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		}
