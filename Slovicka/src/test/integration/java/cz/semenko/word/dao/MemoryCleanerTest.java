d8cd4249d2aae071b7ed7e806be9d64fa3217993
Kyrylo Semenko <semenko.k@seznam.cz>
2014-11-02 09:54:43 +0100

    Tests place refactoring

---
+++ Slovicka/src/test/integration/java/cz/semenko/word/dao/MemoryCleanerTest.java
@@ -0,0 +1,22 @@
+package test.integration.java.cz.semenko.word.dao;
+
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+import cz.semenko.word.ApplicationContextProvider;
+import cz.semenko.word.sleeping.MemoryCleaner;
+
+public class MemoryCleanerTest extends TestCase {
+	public static Logger logger = Logger.getLogger(new Throwable().getStackTrace()[0].getClassName());
+
+	@Test
+	public void testCleanMemoryFromRedundantAssociations () throws Exception {
+		ApplicationContext ctx = ApplicationContextProvider.getIntegrationTestApplicationContext();
+		MemoryCleaner memoryCleaner = ctx.getBean(MemoryCleaner.class);
+		memoryCleaner.cleanMemoryFromRedundantCells();
+	}
+}
